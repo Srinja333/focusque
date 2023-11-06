349376e7b1ff08ab6f487c1854ac41293ffa9e07
Srinjay Kapri <srinjayayan1262000@gmail.com>
2023-11-06 14:21:59 +0530

    design

---
+++ user-frontend/app/pages/login/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDR5a-vRRPzzbrQndl_Nr4xxhERYOBGwjI",
+  authDomain: "focusque-eace3.firebaseapp.com",
+  projectId: "focusque-eace3",
+  storageBucket: "focusque-eace3.appspot.com",
+  messagingSenderId: "912656358835",
+  appId: "1:912656358835:web:06dd8f7de0c80bbe287fc6",
+  measurementId: "G-Z6T31SF3E9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ user-frontend/app/pages/login/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../../shared/constant.module.css"
+
+function Login() {
+  return (
+    <div className={styles.app_style}>Login</div>
+  )
+}
+
+export default Login

a43f9bf40a3ea576d9c43f43d51edfb8eb14ab48
Srinjay Kapri <srinjayayan1262000@gmail.com>
2023-11-06 14:42:44 +0530

    app

--- user-frontend/app/pages/login/page.js
+++ user-frontend/app/pages/login/page.js
@@ -1,10 +1,8 @@
-import React from 'react'
-import styles from "../../shared/constant.module.css"
+import React from "react";
+import styles from "../../shared/constant.module.css";
 
 function Login() {
-  return (
-    <div className={styles.app_style}>Login</div>
-  )
+  return <div className={styles.app_main_style}>Login</div>;
 }
 
-export default Login
+export default Login;
